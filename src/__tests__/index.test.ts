942fd364311a0a5a89470b38a47b461276d7c644
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-06-07 06:57:10 +0300

    Add suggest size limit

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -15,4 +15,12 @@ describe('refresh & guess', () => {
     refresh(['test', 'test2'], [5, 10])
     expect(guess('te')).toEqual(['test2', 'test'])
   })
+
+  it('should give maximum 12 suggests', () => {
+    refresh(
+      ['aa', 'ab', 'ac', 'ad', 'ae', 'af', 'ag', 'ah', 'ai', 'aj', 'ak', 'al', 'am'],
+      [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+    )
+    expect(guess('a')).toEqual(['aa', 'ab', 'ac', 'ad', 'ae', 'af', 'ag', 'ah', 'ai', 'aj', 'ak', 'al'])
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ interface SuggestItem {
   modificationDate: number
 }
 
+const MAX_SUGGEST_SIZE = 12
+
 let suggestItems: SuggestItem[]
 
 export function refresh(functionNames: string[], modificationDates: number[]): void {
@@ -25,4 +27,5 @@ export function guess(start: string): string[] {
       return aDate < bDate ? 1 : -1
     })
     .map(({ functionName }) => functionName)
+    .slice(0, MAX_SUGGEST_SIZE)
 }
