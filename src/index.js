d3351dc358c816493aec50e10fe49dfdde7b2754
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-06-08 20:34:52 +0300

    sort items by date

--- src/index.js
+++ src/index.js
@@ -2,30 +2,19 @@
 
 const { zip } = require('./utils/zip')
 const { createTrie, findInTrie } = require('./trie')
-/* import type { Trie } from './trie' */
 
 const MAX_SUGGEST_SIZE = 12
 
-let trie /*: Trie */
+let trie
 
 function refresh(functionNames /*: string[] */, modificationDates /*: number[] */) {
   const values = zip(functionNames, modificationDates)
-    .map(([name, date]) => ({
-      name,
-      date: date.toString().padStart(13, '0'),
-    }))
+    .map(([name, date]) => ({ name, date }))
   trie = createTrie(values)
 }
 
 function guess(start /*: string */) /*: string[] */ {
   return findInTrie(start, trie)
-    .sort((a, b) => {
-      if (a.date === b.date) {
-        return 0
-      }
-
-      return a.date < b.date ? 1 : -1
-    })
     .map(({ name }) => name)
     .slice(0, MAX_SUGGEST_SIZE)
 }
--- src/trie.js
+++ src/trie.js
@@ -6,26 +6,58 @@ type Value = {|
   date: number,
 |}
 
-type SpectialTrie
-  = { values: Value[] }
-  | { root: true, leaves: Leaves }
-
 type Leaves = { [key: string]: Trie }
 
-export type Trie
-  = { leaves: Leaves } & SpectialTrie
+export type Trie = {
+  leaves?: Leaves,
+  values?: Value[],
+  root?: true,
+}
 */
 
-function helper(rest /*: string */, value /*: Value */, trie /*:? Trie */) /*: Trie */ {
+function compareNames(a /*: Value */, b /*: Value */) /*: 0 | 1 | -1 */ {
+  if (a.name === b.name) {
+    return 0;
+  }
+  return a.name > b.name ? 1 : -1
+}
+
+function compare(a /*: Value */, b /*: Value */) /*: 0 | 1 | -1 */ {
+  if (a.date === b.date) {
+    return compareNames(a, b)
+  }
+  return a.date < b.date ? 1 : -1
+}
+
+function insertIntoSortedArray(value /*: Value */, arr /*: Value[] */) /*: Value[] */ {
+  if (arr.length === 0) {
+    return [value]
+  }
+
+  if (arr.length === 1) {
+    return compare(value, arr[0]) === 1 ? [arr[0], value] : [value, arr[0]]
+  }
+
+  const middleIndex = Math.floor(arr.length / 2)
+  const first = arr.slice(0, middleIndex)
+  const last = arr.slice(middleIndex)
+
+  return compare(value, last[0]) === -1
+    ? [...insertIntoSortedArray(value, first), ...last]
+    : [...first, ...insertIntoSortedArray(value, last)]
+}
+
+function insertValueHelper(rest /*: string */, value /*: Value */, trie /*:? Trie */) /*: Trie */ {
   const newTrie /*: Trie */ = trie && trie.root
-    ? { root: true }
-    : { values: trie ? [...trie.values, value] : [value] }
+    ? { root: true, leaves: {} }
+    : { values: trie && trie.values ? insertIntoSortedArray(value, trie.values) : [value] }
+
   const leaves = trie && trie.leaves;
 
   if (rest) {
     const nameKey = rest[0]
     newTrie.leaves = Object.assign({}, leaves, {
-      [nameKey]: helper(rest.slice(1), value, leaves && leaves[nameKey]),
+      [nameKey]: insertValueHelper(rest.slice(1), value, leaves && leaves[nameKey]),
     })
   }
 
@@ -33,7 +65,7 @@ function helper(rest /*: string */, value /*: Value */, trie /*:? Trie */) /*: T
 }
 
 function insertValue(value /*: Value */, trie /*: Trie */) /*: Trie */ {
-  return helper(value.name, value, trie)
+  return insertValueHelper(value.name, value, trie)
 }
 
 function createTrie(values /*: Value[] */) /*: Trie */ {
@@ -46,7 +78,7 @@ function findInTrie(start /*: string */, trie /*: Trie */) /*: Value[] */ {
       ? trie.leaves[key]
       : { values: [] }
   }, trie)
-  return r.values
+  return r.values || []
 }
 
 module.exports = { createTrie, insertValue, findInTrie }
