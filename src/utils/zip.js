380e551d6ea98f46434480d5f26d0f9c246944bb
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-06-08 09:07:53 +0300

    fix zip util type annotations

--- src/utils/zip.js
+++ src/utils/zip.js
@@ -4,7 +4,7 @@ function zip /*:: <T, U>*/(xs /*: T[]*/, ys /*: U[]*/) /*: Array<[T, U]>*/ {
   if (xs.length !== ys.length) {
     throw new Error('Arrays should have the same length')
   }
-  return xs.map((x, index /*: [T, U]*/) => [x, ys[index]])
+  return xs.map((x, index) /*: [T, U]*/ => [x, ys[index]])
 }
 
 module.exports = { zip }
